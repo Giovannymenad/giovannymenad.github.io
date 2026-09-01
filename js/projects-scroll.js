8d1099bea87430b569a4ce1cee96c683cbe0e8c7
Giovannymenad <giovannymena.design@gmail.com>
2026-09-01 13:42:06 -0500

    Update projects-scroll.js

--- js/projects-scroll.js
+++ js/projects-scroll.js
@@ -38,7 +38,7 @@ mm.add({
   } 
   else if (isMobileLarge) { // <= 425px
     gap = 20; // Espacio más pequeño para móviles grandes
-    extraSpace = 220; 
+    extraSpace = 180; 
   } 
   else if (isMobileMedium) { // <= 375px
     gap = 10;
